872b5b4d8c00ab6b42e2f0ddc5fc7e682ad9a278
Rinat Sadykov <sadykovra@mail.ru>
2020-10-13 14:16:47 +0500

    add menu.js

--- js/menu.js
+++ js/menu.js
@@ -3,14 +3,21 @@
   window.menu = () => {
     const wrappers = Array.from(document.querySelectorAll('.cli-header'))
     wrappers.forEach(w => {
-        const content = w.querySelector('.cli-header-content')
-        const [close, open] = w.querySelectorAll('.cli-header-menu-btn')
-        open.addEventListener('click', () => {
-            content.classList.add('cli-mobile-menu')
-        })
-        close.addEventListener('click', () => {
-          content.classList.remove('cli-mobile-menu')
+      const content = w.querySelector('.cli-header-content')
+      const [close, open] = w.querySelectorAll('.cli-header-menu-btn')
+      open.addEventListener('click', () => {
+          content.classList.add('cli-mobile-menu')
       })
+      close.addEventListener('click', () => {
+        content.classList.remove('cli-mobile-menu')
+      })
+
+      const links = Array.from(w.querySelectorAll('.cli-header-menu a'))
+      const location = window.location.href;
+      const activeLink = links.find(l => l.href === location)
+      if (activeLink) {
+        activeLink.classList.add('active')
+      }
     })
   }
   
