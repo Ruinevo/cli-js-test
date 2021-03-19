07fbb9ad324867af8df8d6f61a111e0344241820
Rinat Sadykov <SadykovRAl@bnipi.rosneft.ru>
2021-03-19 13:27:05 +0500

    test1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
 'use strict'
 window.formApi = 'https://builder-api.hostman.site/api/forms'
 
-console.time('test')
-
-
-
 Object.keys(window.constructorCliFunc).forEach(key => window.constructorCliFunc[key]())
-  console.timeEnd('test')
 
 
 
